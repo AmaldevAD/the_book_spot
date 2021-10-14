f319d710d30e5b27507a1263641d4360c8b1c4df
Akshay P <akshay.p@soti.net>
2021-10-14 17:07:37 +0530

    admin categories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { AdminHeaderComponent } from './components/admin/admin-header/admin-header.component';
 import { AdminBodyComponent } from './components/admin/admin-body/admin-body.component';
+import { CategoriesComponent } from './components/admin/categories/categories.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminComponent,
     AdminHeaderComponent,
-    AdminBodyComponent
+    AdminBodyComponent,
+    CategoriesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
