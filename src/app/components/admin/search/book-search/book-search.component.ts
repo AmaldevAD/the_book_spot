b97807e0d76a6eec1061778bc4f515f7bf2d0c1b
Akshay P <akshay.p@soti.net>
2021-10-15 21:57:51 +0530

    search implemented

---
+++ src/app/components/admin/search/book-search/book-search.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-book-search',
+  templateUrl: './book-search.component.html',
+  styleUrls: ['./book-search.component.css']
+})
+export class BookSearchComponent implements OnInit {
+
+  constructor(private router :Router) { }
+
+  ngOnInit(): void {
+  }
+  public searchTerm:String=""
+
+  search():void{
+    console.log(this.searchTerm)
+
+    if(this.searchTerm)
+    this.router.navigateByUrl('admin/book/search/' + this.searchTerm);
+  }
+}
